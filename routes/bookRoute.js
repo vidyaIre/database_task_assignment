1879069c38ba83d8664c19f2507b20c738315e72
vidya <vidya.palliyalil@gmail.com>
2025-03-10 17:11:12 +0530

    add createBook route with error handling to bookRoute

--- routes/bookRoute.js
+++ routes/bookRoute.js
@@ -1,4 +1,9 @@
+const { createBook } = require('../controllers/bookController');
+const errorHandler = require('../middleware/errorHandler');
 
 const router = require('express').Router();
 
+module.exports = router;
+
+router.post('/createBook', errorHandler, createBook);
 module.exports = router;
